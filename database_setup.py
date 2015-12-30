0408658eb0e54c0cd30fa45fdc28e116a710fefd
kamuri <akamuri@gmail.com>
2015-12-30 15:35:43 -0700

    Created database and added one record . Can view it in http://localhost:5000/

---
+++ database_setup.py
@@ -0,0 +1,88 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+ 
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+       }
+
+
+class Game(Base):
+    __tablename__ = 'game'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    ageRating = Column(String(800), nullable=False)
+    price = Column(String(8))
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+           'description'  : self.description,
+           'ageRating'    : self.ageRating,
+           'price'        : self.price,
+
+       }
+
+
+class Genre(Base):
+    __tablename__ = 'genre'
+   
+    id = Column(Integer, primary_key=True)
+    Genre = Column(String(250), nullable=False)
+    game_id = Column(Integer,ForeignKey('game.id'))
+    game = relationship(Game)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+           'user_id'      : self.game_id,
+       }
+ 
+class Console(Base):
+    __tablename__ = 'console'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable=False)
+    game_id = Column(Integer,ForeignKey('game.id'))
+    game = relationship(Game)
+
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'       : self.name,
+           'id'         : self.id,
+           'game_id'    : self.game_id,
+       }
+
+
+
+engine = create_engine('sqlite:///gameCatalog.db')
+ 
+
+Base.metadata.create_all(engine)
